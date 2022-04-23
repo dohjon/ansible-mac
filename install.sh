c5588cb804b9857dfc6fade2166ddc4de567cb47
Jonathan Doherty <dohjon@users.noreply.github.com>
2022-04-23 23:06:27 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ if [[ $(uname) == "Darwin" ]]; then
     if ! command -v /opt/homebrew/bin/brew &>/dev/null; then
         echo "Installing: homebrew ..."
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-        #eval "$(/opt/homebrew/bin/brew shellenv)"
+        eval "$(/opt/homebrew/bin/brew shellenv)"
     fi
 
     # https://formulae.brew.sh/formula/ansible
@@ -26,4 +26,3 @@ fi
 
 echo "Running: playbook.yml ..."
 ansible-playbook playbook.yml
-#ansible-playbook playbook.yml --ask-become-pass
