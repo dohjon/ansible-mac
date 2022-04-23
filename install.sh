37b2916b1e8f8f7975346c24ab9bb765707ad73c
Jonathan Doherty <dohjon@users.noreply.github.com>
2022-04-23 22:36:59 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,6 +14,7 @@ if [[ $(uname) == "Darwin" ]]; then
     if ! command -v /opt/homebrew/bin/brew &>/dev/null; then
         echo "Installing: homebrew ..."
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        #eval "$(/opt/homebrew/bin/brew shellenv)"
     fi
 
     # https://formulae.brew.sh/formula/ansible
